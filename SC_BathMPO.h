4dc9888eb97b4e3c4a8296313e8f590f00083ec0
Rok Zitko <rok.zitko@ijs.si>
2020-02-06 12:03:25 +0100

    code cleanup: params, reorganization into header/cc

--- SC_BathMPO.h
+++ SC_BathMPO.h
@@ -1,11 +1,9 @@
 #include <vector>
 using namespace itensor;
 
-bool verbose = false;
-
 //fills the MPO tensors
-void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& eps_, 
-                const std::vector<double>& v_, double U_, double Vbar_ )
+void Fill_SCBath_MPO(MPO& H, const std::vector<double>& eps_, 
+                const std::vector<double>& v_, const params &p)
 {
       //QN objects are necessary to have abelian symmetries in MPS
       QN    qn0  ( {"Sz",  0},{"Nf", 0} ),
@@ -42,18 +40,18 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         ITensor& W = H.ref(i);
         Index right = links.at(i);
 
-        W = ITensor(right, sites_.si(i), sites_.siP(i) );
-        W += sites_.op("Id",i) * setElt(right(1));
+        W = ITensor(right, p.sites.si(i), p.sites.siP(i) );
+        W += p.sites.op("Id",i) * setElt(right(1));
 
-        W += sites_.op("Ntot",i)  * setElt(right(2)) * eps_[i-1];
-        W += sites_.op("Nupdn",i) * setElt(right(2)) * U_;
+        W += p.sites.op("Ntot",i)  * setElt(right(2)) * eps_[i-1];
+        W += p.sites.op("Nupdn",i) * setElt(right(2)) * p.U;
 
-        W += sites_.op("Cup*F",i) * setElt(right(3))    * (-1);
-        W += sites_.op("Cdn*F",i) * setElt(right(4))    * (-1);
-        W += sites_.op("Cdagup*F",i) * setElt(right(5)) * (+1);
-        W += sites_.op("Cdagdn*F",i) * setElt(right(6)) * (+1);
+        W += p.sites.op("Cup*F",i) * setElt(right(3))    * (-1);
+        W += p.sites.op("Cdn*F",i) * setElt(right(4))    * (-1);
+        W += p.sites.op("Cdagup*F",i) * setElt(right(5)) * (+1);
+        W += p.sites.op("Cdagdn*F",i) * setElt(right(6)) * (+1);
 
-        if (verbose) std::cout << "using " << eps_[i-1] <<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i-1] <<std::endl;
     }
 
     // sites 2 ... N-1 are matrices
@@ -62,33 +60,33 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         Index left = dag( links.at(i-1) );
         Index right = links.at(i);
 
-        W = ITensor(left, right, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, right, p.sites.si(i), p.sites.siP(i) );
 
-        W += sites_.op("Id",i) * setElt(left(1), right(1));
+        W += p.sites.op("Id",i) * setElt(left(1), right(1));
 
-        W += sites_.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i-1];
-        W += sites_.op("Nupdn",i)          * setElt(left(1),right(2)) * (Vbar_);
+        W += p.sites.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i-1];
+        W += p.sites.op("Nupdn",i)          * setElt(left(1),right(2)) * p.g;
 
-        W += sites_.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * Vbar_;
-        W += sites_.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * Vbar_;
+        W += p.sites.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * p.g;
+        W += p.sites.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * p.g;
 
-        W += sites_.op("Id",i)*setElt(left(2),right(2));
-        W += sites_.op("F" ,i)*setElt(left(3),right(3));
-        W += sites_.op("F" ,i)*setElt(left(4),right(4));
-        W += sites_.op("F" ,i)*setElt(left(5),right(5));
-        W += sites_.op("F" ,i)*setElt(left(6),right(6));
-        W += sites_.op("Id",i)*setElt(left(7),right(7));
-        W += sites_.op("Id",i)*setElt(left(8),right(8));
+        W += p.sites.op("Id",i)*setElt(left(2),right(2));
+        W += p.sites.op("F" ,i)*setElt(left(3),right(3));
+        W += p.sites.op("F" ,i)*setElt(left(4),right(4));
+        W += p.sites.op("F" ,i)*setElt(left(5),right(5));
+        W += p.sites.op("F" ,i)*setElt(left(6),right(6));
+        W += p.sites.op("Id",i)*setElt(left(7),right(7));
+        W += p.sites.op("Id",i)*setElt(left(8),right(8));
 
-        W += sites_.op("Cdagup",i)*setElt(left(3),right(2))* v_[i-1];
-        W += sites_.op("Cdagdn",i)*setElt(left(4),right(2))* v_[i-1];
-        W += sites_.op("Cup",   i)*setElt(left(5),right(2))* v_[i-1];
-        W += sites_.op("Cdn",   i)*setElt(left(6),right(2))* v_[i-1];
+        W += p.sites.op("Cdagup",i)*setElt(left(3),right(2))* v_[i-1];
+        W += p.sites.op("Cdagdn",i)*setElt(left(4),right(2))* v_[i-1];
+        W += p.sites.op("Cup",   i)*setElt(left(5),right(2))* v_[i-1];
+        W += p.sites.op("Cdn",   i)*setElt(left(6),right(2))* v_[i-1];
 
-        W += sites_.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
-        W += sites_.op("Cdn*Cup",i)      *setElt(left(8),right(2));
+        W += p.sites.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
+        W += p.sites.op("Cdn*Cup",i)      *setElt(left(8),right(2));
 
-        if (verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
     }
 
     //site N is a vector again
@@ -97,21 +95,21 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         ITensor& W = H.ref(i);
         Index left = dag( links.at(i-1) );
 
-        W = ITensor(left, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, p.sites.si(i), p.sites.siP(i) );
         
-        W += sites_.op("Ntot",  i) * setElt(left(1)) * eps_[i-1];
-        W += sites_.op("Nupdn",i)  * setElt(left(1)) * (Vbar_);
+        W += p.sites.op("Ntot",  i) * setElt(left(1)) * eps_[i-1];
+        W += p.sites.op("Nupdn",i)  * setElt(left(1)) * p.g;
 
-        W += sites_.op("Id",    i) * setElt(left(2)) ;
-        W += sites_.op("Cdagup",i) * setElt(left(3)) * v_[i-1];
-        W += sites_.op("Cdagdn",i) * setElt(left(4)) * v_[i-1];
-        W += sites_.op("Cup",   i) * setElt(left(5)) * v_[i-1];
-        W += sites_.op("Cdn",   i) * setElt(left(6)) * v_[i-1];
+        W += p.sites.op("Id",    i) * setElt(left(2)) ;
+        W += p.sites.op("Cdagup",i) * setElt(left(3)) * v_[i-1];
+        W += p.sites.op("Cdagdn",i) * setElt(left(4)) * v_[i-1];
+        W += p.sites.op("Cup",   i) * setElt(left(5)) * v_[i-1];
+        W += p.sites.op("Cdn",   i) * setElt(left(6)) * v_[i-1];
 
-        W += sites_.op("Cdagup*Cdagdn",i) * setElt(left(7));
-        W += sites_.op("Cdn*Cup",      i) * setElt(left(8));
+        W += p.sites.op("Cdagup*Cdagdn",i) * setElt(left(7));
+        W += p.sites.op("Cdn*Cup",      i) * setElt(left(8));
 
-        if (verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
     }   
     
   }
--- SC_BathMPO_MiddleImp.h
+++ SC_BathMPO_MiddleImp.h
@@ -1,19 +1,17 @@
 #include <vector>
 using namespace itensor;
 
-bool verbose = false;
-
 //fills the MPO tensors
-void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& eps_, 
-                const std::vector<double>& v_, double U_, double Vbar_ )
+void Fill_SCBath_MPO(MPO& H, const std::vector<double>& eps_,
+                const std::vector<double>& v_, const params &p)
 {
       //QN objects are necessary to have abelian symmetries in MPS
       // automatically find the correct values
-      QN    qn0  = - div( sites_.op( "Id",      1) ),
-            cupC = - div( sites_.op( "Cdagup",  1) ),
-            cdnC = - div( sites_.op( "Cdagdn",  1) ),
-            cupA = - div( sites_.op( "Cup",     1) ),
-            cdnA = - div( sites_.op( "Cdn",     1) );
+      QN    qn0  = - div( p.sites.op( "Id",      1) ),
+            cupC = - div( p.sites.op( "Cdagup",  1) ),
+            cdnC = - div( p.sites.op( "Cdagdn",  1) ),
+            cupA = - div( p.sites.op( "Cup",     1) ),
+            cdnA = - div( p.sites.op( "Cdn",     1) );
 
 
     std::vector<Index> links;
@@ -61,24 +59,24 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         ITensor& W = H.ref(i);
         Index right = links.at(i);
 
-        W = ITensor(right, sites_.si(i), sites_.siP(i) );
-        W += sites_.op("Id",i) * setElt(right(1));
+        W = ITensor(right, p.sites.si(i), p.sites.siP(i) );
+        W += p.sites.op("Id",i) * setElt(right(1));
 
         // local H on site
-        W += sites_.op("Ntot",i)  * setElt(right(2)) * eps_[i]; // here use index i
-        W += sites_.op("Nupdn",i) * setElt(right(2)) * (Vbar_);
+        W += p.sites.op("Ntot",i)  * setElt(right(2)) * eps_[i]; // here use index i
+        W += p.sites.op("Nupdn",i) * setElt(right(2)) * p.g;
 
         //hybridization
-        W += sites_.op("Cdagup*F",i) * setElt(right(3))* (+v_[i]); // here use index i 
-        W += sites_.op("Cdagdn*F",i) * setElt(right(4))* (+v_[i]); // here use index i
-        W += sites_.op("Cup*F",   i) * setElt(right(5))* (-v_[i]); // here use index i
-        W += sites_.op("Cdn*F",   i) * setElt(right(6))* (-v_[i]); // here use index i
+        W += p.sites.op("Cdagup*F",i) * setElt(right(3))* (+v_[i]); // here use index i 
+        W += p.sites.op("Cdagdn*F",i) * setElt(right(4))* (+v_[i]); // here use index i
+        W += p.sites.op("Cup*F",   i) * setElt(right(5))* (-v_[i]); // here use index i
+        W += p.sites.op("Cdn*F",   i) * setElt(right(6))* (-v_[i]); // here use index i
 
         //SC pairing
-        W += sites_.op("Cdn*Cup",       i) * setElt(right(7)) * Vbar_;
-        W += sites_.op("Cdagup*Cdagdn", i) * setElt(right(8)) * Vbar_;
+        W += p.sites.op("Cdn*Cup",       i) * setElt(right(7)) * p.g;
+        W += p.sites.op("Cdagup*Cdagdn", i) * setElt(right(8)) * p.g;
 
-        if (verbose) std::cout << "using " << eps_[i] << " and "<<v_[i]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i] << " and "<<v_[i]<<std::endl;
     }
 
     // sites 2 ... impSite-1 are matrices
@@ -87,38 +85,38 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         Index left = dag( links.at(i-1) );
         Index right = links.at(i);
 
-        W = ITensor(left, right, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, right, p.sites.si(i), p.sites.siP(i) );
 
-        W += sites_.op("Id",i) * setElt(left(1), right(1));
+        W += p.sites.op("Id",i) * setElt(left(1), right(1));
 
         // local H on site
-        W += sites_.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i]; // here use index i
-        W += sites_.op("Nupdn",i)          * setElt(left(1),right(2)) * (Vbar_);
+        W += p.sites.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i]; // here use index i
+        W += p.sites.op("Nupdn",i)          * setElt(left(1),right(2)) * p.g;
 
         // hybridizations 
-        W += sites_.op("Cdagup*F",i)*setElt(left(1),right(3))* (+v_[i] ); // here use index i 
-        W += sites_.op("Cdagdn*F",i)*setElt(left(1),right(4))* (+v_[i] ); // here use index i
-        W += sites_.op("Cup*F",   i)*setElt(left(1),right(5))* (-v_[i] ); // here use index i
-        W += sites_.op("Cdn*F",   i)*setElt(left(1),right(6))* (-v_[i] ); // here use index i
+        W += p.sites.op("Cdagup*F",i)*setElt(left(1),right(3))* (+v_[i] ); // here use index i 
+        W += p.sites.op("Cdagdn*F",i)*setElt(left(1),right(4))* (+v_[i] ); // here use index i
+        W += p.sites.op("Cup*F",   i)*setElt(left(1),right(5))* (-v_[i] ); // here use index i
+        W += p.sites.op("Cdn*F",   i)*setElt(left(1),right(6))* (-v_[i] ); // here use index i
 
         //SC pairing 
-        W += sites_.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * Vbar_;
-        W += sites_.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * Vbar_;
+        W += p.sites.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * p.g;
+        W += p.sites.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * p.g;
 
         // keep terms
-        W += sites_.op("Id",i)*setElt(left(2),right(2));
-        W += sites_.op("F" ,i)*setElt(left(3),right(3));
-        W += sites_.op("F" ,i)*setElt(left(4),right(4));
-        W += sites_.op("F" ,i)*setElt(left(5),right(5));
-        W += sites_.op("F" ,i)*setElt(left(6),right(6));
-        W += sites_.op("Id",i)*setElt(left(7),right(7));
-        W += sites_.op("Id",i)*setElt(left(8),right(8));
+        W += p.sites.op("Id",i)*setElt(left(2),right(2));
+        W += p.sites.op("F" ,i)*setElt(left(3),right(3));
+        W += p.sites.op("F" ,i)*setElt(left(4),right(4));
+        W += p.sites.op("F" ,i)*setElt(left(5),right(5));
+        W += p.sites.op("F" ,i)*setElt(left(6),right(6));
+        W += p.sites.op("Id",i)*setElt(left(7),right(7));
+        W += p.sites.op("Id",i)*setElt(left(8),right(8));
 
         // add SC pairing terms
-        W += sites_.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
-        W += sites_.op("Cdn*Cup",i)      *setElt(left(8),right(2));
+        W += p.sites.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
+        W += p.sites.op("Cdn*Cup",i)      *setElt(left(8),right(2));
 
-        if (verbose) std::cout << "using " << eps_[i] << " and "<<v_[i]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i] << " and "<<v_[i]<<std::endl;
     }
 
 
@@ -129,33 +127,33 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         Index left = dag( links.at(i-1) );
         Index right = links.at(i);
 
-        W = ITensor(left, right, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, right, p.sites.si(i), p.sites.siP(i) );
 
-        W += sites_.op("Id",i) * setElt(left(1), right(1));
+        W += p.sites.op("Id",i) * setElt(left(1), right(1));
 
-        W += sites_.op("Ntot",i)  * setElt(left(1), right(2)) * eps_[0]; // use index 0
-        W += sites_.op("Nupdn",i) * setElt(left(1), right(2)) * U_;
+        W += p.sites.op("Ntot",i)  * setElt(left(1), right(2)) * eps_[0]; // use index 0
+        W += p.sites.op("Nupdn",i) * setElt(left(1), right(2)) * p.U;
         
         // hybridizations
-        W += sites_.op("Cup*F",    i)*setElt(left(1),right(3)) * (-1);
-        W += sites_.op("Cdn*F",    i)*setElt(left(1),right(4)) * (-1);
-        W += sites_.op("Cdagup*F", i)*setElt(left(1),right(5)) * (+1);
-        W += sites_.op("Cdagdn*F", i)*setElt(left(1),right(6)) * (+1);
+        W += p.sites.op("Cup*F",    i)*setElt(left(1),right(3)) * (-1);
+        W += p.sites.op("Cdn*F",    i)*setElt(left(1),right(4)) * (-1);
+        W += p.sites.op("Cdagup*F", i)*setElt(left(1),right(5)) * (+1);
+        W += p.sites.op("Cdagdn*F", i)*setElt(left(1),right(6)) * (+1);
 
 
-        W += sites_.op("Id",i) * setElt(left(2), right(2));
+        W += p.sites.op("Id",i) * setElt(left(2), right(2));
 
         // hybridizations
-        W += sites_.op("Cup",    i)*setElt(left(3),right(2));
-        W += sites_.op("Cdn",    i)*setElt(left(4),right(2));
-        W += sites_.op("Cdagup", i)*setElt(left(5),right(2));
-        W += sites_.op("Cdagdn", i)*setElt(left(6),right(2));
+        W += p.sites.op("Cup",    i)*setElt(left(3),right(2));
+        W += p.sites.op("Cdn",    i)*setElt(left(4),right(2));
+        W += p.sites.op("Cdagup", i)*setElt(left(5),right(2));
+        W += p.sites.op("Cdagdn", i)*setElt(left(6),right(2));
 
         //keep SC pairing
-        W += sites_.op("Id",i) *setElt(left(7),right(7)) ;
-        W += sites_.op("Id",i) *setElt(left(8),right(8)) ;
+        W += p.sites.op("Id",i) *setElt(left(7),right(7)) ;
+        W += p.sites.op("Id",i) *setElt(left(8),right(8)) ;
 
-        if (verbose) std::cout << "using " << eps_[0] <<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[0] <<std::endl;
     }
 
 
@@ -165,33 +163,33 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         Index left = dag( links.at(i-1) );
         Index right = links.at(i);
 
-        W = ITensor(left, right, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, right, p.sites.si(i), p.sites.siP(i) );
 
-        W += sites_.op("Id",i) * setElt(left(1), right(1));
+        W += p.sites.op("Id",i) * setElt(left(1), right(1));
 
-        W += sites_.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i-1];  // use index i-1
-        W += sites_.op("Nupdn",i)          * setElt(left(1),right(2)) * (Vbar_);
+        W += p.sites.op("Ntot",i)           * setElt(left(1),right(2)) * eps_[i-1];  // use index i-1
+        W += p.sites.op("Nupdn",i)          * setElt(left(1),right(2)) * p.g;
 
-        W += sites_.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * Vbar_;
-        W += sites_.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * Vbar_;
+        W += p.sites.op("Cdn*Cup",i)        * setElt(left(1),right(7)) * p.g;
+        W += p.sites.op("Cdagup*Cdagdn",i)  * setElt(left(1),right(8)) * p.g;
 
-        W += sites_.op("Id",i)*setElt(left(2),right(2));
-        W += sites_.op("F" ,i)*setElt(left(3),right(3));
-        W += sites_.op("F" ,i)*setElt(left(4),right(4));
-        W += sites_.op("F" ,i)*setElt(left(5),right(5));
-        W += sites_.op("F" ,i)*setElt(left(6),right(6));
-        W += sites_.op("Id",i)*setElt(left(7),right(7));
-        W += sites_.op("Id",i)*setElt(left(8),right(8));
+        W += p.sites.op("Id",i)*setElt(left(2),right(2));
+        W += p.sites.op("F" ,i)*setElt(left(3),right(3));
+        W += p.sites.op("F" ,i)*setElt(left(4),right(4));
+        W += p.sites.op("F" ,i)*setElt(left(5),right(5));
+        W += p.sites.op("F" ,i)*setElt(left(6),right(6));
+        W += p.sites.op("Id",i)*setElt(left(7),right(7));
+        W += p.sites.op("Id",i)*setElt(left(8),right(8));
 
-        W += sites_.op("Cdagup",i)*setElt(left(3),right(2))* v_[i-1];  // use index i-1
-        W += sites_.op("Cdagdn",i)*setElt(left(4),right(2))* v_[i-1];  // use index i-1
-        W += sites_.op("Cup",   i)*setElt(left(5),right(2))* v_[i-1];  // use index i-1
-        W += sites_.op("Cdn",   i)*setElt(left(6),right(2))* v_[i-1];  // use index i-1
+        W += p.sites.op("Cdagup",i)*setElt(left(3),right(2))* v_[i-1];  // use index i-1
+        W += p.sites.op("Cdagdn",i)*setElt(left(4),right(2))* v_[i-1];  // use index i-1
+        W += p.sites.op("Cup",   i)*setElt(left(5),right(2))* v_[i-1];  // use index i-1
+        W += p.sites.op("Cdn",   i)*setElt(left(6),right(2))* v_[i-1];  // use index i-1
 
-        W += sites_.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
-        W += sites_.op("Cdn*Cup",i)      *setElt(left(8),right(2));
+        W += p.sites.op("Cdagup*Cdagdn",i)*setElt(left(7),right(2));
+        W += p.sites.op("Cdn*Cup",i)      *setElt(left(8),right(2));
 
-        if (verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
     }
 
     //site N is a vector again - same as before
@@ -200,21 +198,21 @@ void Fill_SCBath_MPO(MPO& H, const SiteSet& sites_, const std::vector<double>& e
         ITensor& W = H.ref(i);
         Index left = dag( links.at(i-1) );
 
-        W = ITensor(left, sites_.si(i), sites_.siP(i) );
+        W = ITensor(left, p.sites.si(i), p.sites.siP(i) );
         
-        W += sites_.op("Ntot",  i) * setElt(left(1)) * eps_[i-1]; // use index i-1
-        W += sites_.op("Nupdn",i)  * setElt(left(1)) * (Vbar_);
+        W += p.sites.op("Ntot",  i) * setElt(left(1)) * eps_[i-1]; // use index i-1
+        W += p.sites.op("Nupdn",i)  * setElt(left(1)) * p.g;
 
-        W += sites_.op("Id",    i) * setElt(left(2)) ;
-        W += sites_.op("Cdagup",i) * setElt(left(3)) * v_[i-1];  // use index i-1
-        W += sites_.op("Cdagdn",i) * setElt(left(4)) * v_[i-1];  // use index i-1
-        W += sites_.op("Cup",   i) * setElt(left(5)) * v_[i-1];  // use index i-1
-        W += sites_.op("Cdn",   i) * setElt(left(6)) * v_[i-1];  // use index i-1
+        W += p.sites.op("Id",    i) * setElt(left(2)) ;
+        W += p.sites.op("Cdagup",i) * setElt(left(3)) * v_[i-1];  // use index i-1
+        W += p.sites.op("Cdagdn",i) * setElt(left(4)) * v_[i-1];  // use index i-1
+        W += p.sites.op("Cup",   i) * setElt(left(5)) * v_[i-1];  // use index i-1
+        W += p.sites.op("Cdn",   i) * setElt(left(6)) * v_[i-1];  // use index i-1
 
-        W += sites_.op("Cdagup*Cdagdn",i) * setElt(left(7));
-        W += sites_.op("Cdn*Cup",      i) * setElt(left(8));
+        W += p.sites.op("Cdagup*Cdagdn",i) * setElt(left(7));
+        W += p.sites.op("Cdn*Cup",      i) * setElt(left(8));
 
-        if (verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
+        if (p.verbose) std::cout << "using " << eps_[i-1] << " and "<<v_[i-1]<<std::endl;
     }   
     
   }
